0031474398afccd70b1137c30c9bd18c92b16bf0
Andres Pina Fonseca <apina@icck.net.co>
2016-05-28 15:24:40 -0500

    compress file

--- includes/own/code_debugger.js
+++
@@ -1,7 +0,0 @@
-var editor = CodeMirror.fromTextArea(document.getElementById("textarea-debug"), {
-	lineNumbers: true,
-	matchBrackets: true,
-	mode: "text/x-php",
-	indentUnit: 4,
-	indentWithTabs: true
-});
